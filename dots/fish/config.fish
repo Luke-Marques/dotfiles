610c6204462a70e297ad9e97cb2c24380eb212b9
Luke Marques <accounts@lukemarques.com>
2025-04-26 18:49:53 +0000

    fish: Update config.

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,1 +1,11 @@
 starship init fish | source
+
+# Ensure your critical universals exist
+set -qU XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
+set -qU XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
+set -qU XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
+
+# Favor globals for most things
+set -gx HELIX_RUNTIME ~/helix/runtime
+set -gx EDITOR hx
+set -gx VISUAL code
